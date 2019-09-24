d31890432d4ccadc85b9e0c653ca4c4bdf520b5b
danie <danie@DESKTOP-O5UNK6I>
2019-09-24 08:32:27 +0200

    Primer comit

---
+++ src/prueba03/Prueba03.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prueba03;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+
+/**
+ *
+ * @author daniel
+ */
+public class Prueba03 {
+    private static Object stringToRead;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Usa la funcion de debajo
+        try{
+            ReadCharacters();
+        }catch(IOException e){
+            System.out.println("Error");
+        }
+        
+    }
+    /*
+        @param 
+    */
+    public static void ReadCharacters()throws IOException{
+        //Crear StringBuilder 
+        StringBuilder stringLeer = new StringBuilder();
+        //Se le añade al final de cada salto la linea de texto que le introducimos
+        stringLeer.append("Hola Mundo, como estas hoy? ");
+        stringLeer.append("Comienzo de segunda linea");
+        stringLeer.append("Comienzo tercera linea");
+
+        /*
+            Utilizar un try/catch para usar la función ready de StringReader que
+            devuelve un boolean que permite o no saber si el 
+        */
+         //Creacion de String reader en el cual se le pasa el StringBuilder en ToString y luego lo imprime por pantalla
+            StringReader reader = new StringReader(stringLeer.toString());
+            
+            /*
+            Utilizar un try/catch para usar la función ready de StringReader.read que 
+            debuelve un -1 si llega al final del array de caracteres            
+            */            
+            int k=0;  
+            while((k=reader.read())!=-1){  
+                System.out.print((char)k);  
+            }  
+            
+                
+            
+    }        
+       
+}
+    
+
